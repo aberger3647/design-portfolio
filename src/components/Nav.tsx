1699b9ccc793c5f5df9a4e57f856946d35059f8b
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-10-07 20:14:00 -0500

    add tailwind and make nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
 export const Nav = () => {
   return (
-    <nav>
-      <p>nav</p>
+    <nav className='flex h-16 justify-between p-4 border border-b-1'>
+      <h2 className='flex items-center'>Alex Berger</h2>
+      <ul className='flex gap-4 items-center'>
+        <li>Motion Design</li>
+        <li>Illustration</li>
+        <li>About</li>
+      </ul>
     </nav>
   );
 };
