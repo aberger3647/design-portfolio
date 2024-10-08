3f2a8e2ac917e16f81eec56d4b3a6eae47aa6f5d
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-10-07 22:02:37 -0500

    create about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,37 @@
+import headshot from "../assets/headshotsmall.jpg";
+
 export const About = () => {
   return (
-      <p>about</p>
+    <div className="main">
+      <div className="flex sm:flex-row flex-col">
+      <div className="max-w-96 sm:mr-4 mb-4">
+          <img src={headshot} alt="Black and white headshot of Alex" />
+        </div>
+
+        <div className="max-w-96">
+          <p>
+            Hello! I am an animator, illustrator, graphic designer, video
+            editor, and web designer & developer based in Austin, Texas.
+          </p>
+          <br />
+          <p>
+            I earned a BFA in Communication Design with a minor in Writing from
+            Washington University in St. Louis, as well as a Full Stack coding
+            certificate from the University of Texas at Austin.
+          </p>
+          <br />
+          <p>As a designer, I use Adobe Illustrator, Photoshop, After Effects, XD and Figma to create social media content, videos, assets, motion graphics templates, websites and user interfaces.</p>
+          <br />
+          <p>As a developer, I use React, Express, Node, MongoDB, JavaScript, TypeScript, MySQL and many other frameworks/libraries to transform my designs into fully-functioning full-stack applications.</p>
+          <br />
+          <p>
+            When I'm not finessing the graph editor or coding, I am <a href='https://www.bigalstrength.com/' target='_blank'>training
+            myself and others at the gym</a>.
+          </p>
+          <br />
+          <p>Contact: aberger3647@gmail.com</p>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const Nav = () => {
   return (
     <nav className='flex h-16 justify-between p-4 border border-b-1'>
       <h2 className='flex items-center'>Alex Berger</h2>
       <ul className='flex gap-4 items-center'>
-        <li>Motion Design</li>
-        <li>Illustration</li>
-        <li>About</li>
+        <li><Link to='/'>Motion Design</Link></li>
+        <li><Link to='/illustration'>Illustration</Link></li>
+        <li><Link to='/about'>About</Link></li>
       </ul>
     </nav>
   );
